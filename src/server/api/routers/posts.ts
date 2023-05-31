5c81a955c96488b6df0f830caed41591dec31136
pat <patgiok@gmail.com>
2023-05-30 21:02:54 -0700

    fixed undefined author in PostWithUser type correctly with TRPCError, (gives correct status + etc)

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,6 @@
 import type { User } from "@clerk/nextjs/dist/server";
 import { clerkClient } from "@clerk/nextjs/dist/server-helpers.server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -26,9 +27,17 @@ export const postRouter = createTRPCRouter({
 
     console.log(users);
 
-    return posts.map((post) => ({
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId)
+
+      if(!author) throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author for post not found",
+      })
+
+      return {
       post,
-      author: users.find((user) => user.id === post.authorId)!,
-    }))
+      author,
+    }})
   }),
 });
